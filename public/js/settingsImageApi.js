230231f1a1cc8d62bc3294d9930104a2ae35c09f
TheBanusco10 <thebanusco10@gmail.com>
2021-06-01 17:54:46 +0200

    Cambio en API fondo

--- public/js/settingsImageApi.js
+++ public/js/settingsImageApi.js
@@ -11,15 +11,13 @@ $(function () {
         let imagenABuscar = $('#buscarImagen').val();
 
         if (imagenABuscar != '' && imagenABuscar.length >= 3) {
-            $.get(`https://pixabay.com/api/?key=18966501-51e9929a6ba9713e742f5f513&q=${imagenABuscar}&safesearch=true`, function(data) {
-                let {hits: images} = data;
-
-                console.log(images);
+            $.get(`https://api.unsplash.com/search/photos?page=1&query=${imagenABuscar}&client_id=1GNzdWkEGLA7kITUfTGoi_GlUoUW7u8-bslQ-XzJhgw`, function(data) {
+                let {results: images} = data;
 
                 $('#resultadosImagen').html(
                     images.map(element => {
-                        return `<div id="${element.id}" class="imagen" data-url="${element.largeImageURL}">
-                                    <img src="${element.previewURL}" alt="fondo">
+                        return `<div id="${element.id}" class="imagen" data-url="${element.urls.full}">
+                                    <img src="${element.urls.thumb}" alt="fondo">
                                 </div>`
                     })
                 );
@@ -31,7 +29,6 @@ $(function () {
 
     $('#resultadosImagen').on('click', '.imagen', function () {
 
-        console.log($(this).attr('id'));
         setImage($(this).data('url'));
 
     });

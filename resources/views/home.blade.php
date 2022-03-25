3da2e4941af3502a469414a2bb48ca5834c7a639
TheBanusco10 <thebanusco10@gmail.com>
2022-03-25 15:57:39 +0100

    Design changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@
 
                                 <!-- No hay resultados -->
                                 @if (count($resultados) == 0)
-                                    <p>Sin resultados</p>
+                                    <h4 class="text-light text-center">Sin resultados</h4>
                                 @else
                                 <!-- Sí hay resultados y los mostramos -->
                                     <div>
@@ -132,10 +132,10 @@
                                         @endforeach
                                     </div>
 
-                                    <div class="mt-3 text-center">
-                                        <a class="btn btn-primary" href="{{ route('home') }}">Todas tus contraseñas</a>
-                                    </div>
-                                @endif
+                                    @endif
+                                <div class="mt-3 text-center">
+                                    <a class="btn btn-primary" href="{{ route('home') }}">Todas tus contraseñas</a>
+                                </div>
                             @else
                                 <div class="row justify-content-center passwords mt-2">
                                     @foreach($passwords as $password)
@@ -212,18 +212,68 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row justify-content-center passwords mt-2">
-                                    @if ($buscandoCard)
-                                        @if (count($resultados) == 0)
-                                            <p class="text-light">Sin resultados</p>
-                                        @else
-                                            <div class="col-12">
-                                                <h4 class="mb-3">Resultados</h4>
-                                            </div>
-                                        @endif
-
+                                
+                                @if ($buscandoCard)
+                                    @if (count($resultados) == 0)
+                                        <h4 class="text-light text-center">Sin resultados</h4>
                                     @else
+                                        <div class="col-12">
+                                            <h4 class="mb-3 text-light text-center">Resultados</h4>
+                                        </div>
                                         
+                                        <div class="row justify-content-center passwords mt-2">
+                                            @foreach($resultados as $resultado)
+
+                                                <div class="p-3 p-0 custom-card">
+                                                    <h4>
+                                                        {{$resultado->title}}
+                                                    </h4>
+                                                    <p class="card_number"> {{$resultado->card_number}} </p>
+                                                    <p> {{$resultado->expiration}} </p>
+                                                    <input readonly type="password" id="userPassword" name="{{ $resultado->id }}" class="form-control" value="{{ $resultado->ccv }}" aria-label="Contraseña del usuario" aria-describedby="button-addon2">
+                                                    <div class="acciones">
+
+                                                        <a title="Editar" href="{{ route('editCard', $resultado) }}" class="btn btn-outline-success">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                                            </svg>
+                                                        </a>
+                                                        <button title="Copiar" class="btn btn-outline-secondary copyPassword" data-id="{{ $resultado->id }}">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                                                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                                                            </svg>
+                                                        </button>
+                                                        <button title="Mostrar" data-id="{{ $resultado->id }}" class="btn btn-outline-primary showPassword">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                                                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                                                            </svg>
+                                                        </button>
+
+                                                        <button type="submit" onclick="confirmarEliminar({{ $resultado->id }}, 'tarjeta')" title="Borrar tarjeta" id="borrarBoton" class="btn btn-outline-danger">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+
+                                            @endforeach
+                                        </div>
+                                        
+                                    @endif
+
+                                    <div class="mt-3 text-center">
+                                        <a class="btn btn-primary" href="{{ route('home') }}">Todas tus tarjetas</a>
+                                    </div>
+
+                                @else
+                                    <div class="row justify-content-center passwords mt-2">
+                                    
                                         @foreach($credit_cards as $resultado)
 
                                             <div class="p-3 p-0 custom-card">
@@ -265,22 +315,22 @@
 
 
                                         @endforeach
-
-
-                                    @endif
-                                </div>
-                            </div>
-
-                        @else
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-12 text-center pt-3">
-                                        <h3>Para acceder a esta funcionalidad actualiza tu cuenta a Pro</h3>
                                     </div>
+
+                                @endif
+                            </div>
+                        </div>
+
+                    @else
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 text-center pt-3">
+                                    <h3>Para acceder a esta funcionalidad actualiza tu cuenta a Pro</h3>
                                 </div>
                             </div>
-                        @endif
-                    </div>
+                        </div>
+                    @endif
+
 
                     <div class="tab-pane fade" id="pro" role="tabpanel" aria-labelledby="pro">
                         <div class="col-12">
